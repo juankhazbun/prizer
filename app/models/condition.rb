2191441895463b41767166b02158c85ee8c9c1e2
Juan Carlos Hazbun Nieto <jhazbun@disetronica.com>
2015-01-29 15:06:30 -0500

    Adding validations to the conditions model. Adding tests for conditions model.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,3 +1,5 @@
 class Condition < ActiveRecord::Base
-  belongs_to :prize
+  belongs_to :prize  
+  validates :criteria, presence: true
+  validates :offset, numericality: { only_integer: true }
 end
--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Condition, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context 'is invalid' do
+    
+    before :all do
+        @condition = Condition.new
+      end
+    
+    it 'when description is empty' do
+      @condition.criteria = ''
+      should_not be_valid
+    end
+    
+    it 'when offset is not numeric' do
+        @condition.offset = 'a'
+        should_not be_valid
+      end
+  end
+  
 end
