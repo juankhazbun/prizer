805c18300289234e01f4ed7a49a21843f092a155
Juan Carlos Hazbun Nieto <jhazbun@disetronica.com>
2015-02-10 17:51:00 -0500

    Commenting validation of the offset of the condition.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,5 +1,5 @@
 class Condition < ActiveRecord::Base
   belongs_to :prize  
   validates :criteria, presence: true
-  validates :offset, numericality: { only_integer: true }
+  #validates :offset, numericality: { only_integer: true }
 end
