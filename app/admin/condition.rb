e58a7d6f91dc4091b8bf125774cf4745803e2b54
Juan Carlos Hazbun Nieto <jhazbun@disetronica.com>
2015-02-10 17:48:39 -0500

    Changing values of the condition types to the corresponding symbole.

--- app/admin/condition.rb
+++ app/admin/condition.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Condition do
   form do |f|
     inputs 'Conditions' do
       input :prize_id, as: :select, collection: Prize.all.map { |p| [p.description, p.id] }
-      input :cond_type, as: :select, include_blank: false, collection: [['multiples', '%'], ['less than', '2'], ['greater than', '3'], ['list', '4']]
+      input :cond_type, as: :select, include_blank: false, collection: [['multiples', '%'], ['less than', '<'], ['greater than', '>'], ['list', '4']]
       li "For list of subscribers, add the numbers of the subscribers separated by a comma(,)"
       input :criteria
       input :offset, label: 'After'
