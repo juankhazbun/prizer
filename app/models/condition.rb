042404a94d9319b8db9261aa8174f8b90c246697
Juan Carlos Hazbun Nieto <jhazbun@disetronica.com>
2015-02-25 07:40:37 -0500

    Fixing condition validations.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,5 +1,9 @@
 class Condition < ActiveRecord::Base
   belongs_to :prize  
-  validates :criteria, presence: true
-  #validates :offset, numericality: { only_integer: true }
+  
+  validates :prize, presence: true
+  validates :cond_type, presence: true, inclusion: { in: ['%', '<', '>', 'list'] }
+  VALID_LIST_REGEX = /\A(\d+)(,\s*\d+)*/i
+  validates :criteria, presence: true, format: { with: VALID_LIST_REGEX }
+  validates :offset, allow_blank: true, numericality: { only_integer: true }
 end
