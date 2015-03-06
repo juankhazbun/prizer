ba6b92089e8404a809a0604f5018b23dbd0ac82d
Juan Carlos Hazbun Nieto <jhazbun@disetronica.com>
2015-03-06 08:53:03 -0500

    Adding feature specs for prizes.

--- spec/factories/prizes.rb
+++ spec/factories/prizes.rb
@@ -2,6 +2,11 @@ FactoryGirl.define do
   factory :prize do
     description "Prize #1"
     existences 1
+    
+    factory :new_prize do
+      description "New Prize #1"
+      existences 2
+    end
   end
 
 end
