f76ecc9c81fe7ab9a5cc35c454f3f2ba69eea016
Juan Carlos Hazbun Nieto <jhazbun@disetronica.com>
2015-03-10 14:23:37 -0500

    Customizing condition#index of admin.

--- app/admin/condition.rb
+++ app/admin/condition.rb
@@ -1,7 +1,28 @@
 ActiveAdmin.register Condition do
 
   permit_params :prize_id, :cond_type, :criteria, :offset
+  
+  index do
+    id_column
+    column  "Condition" do |condition|
+      types = {'%' => 'multiples of', '<' => 'less than', '>' => 'greater than'}
+      msg = ""
+      if condition.cond_type == "list"  
+        msg = "Wins prize specific subscribers. Subscriber ##{condition.criteria.split(',').to_sentence(words_connector: ', #', two_words_connector: ' and #', last_word_connector: ' and #')}"
+      else
+        msg = "Wins prize subscribers #{types[condition.cond_type]} #{condition.criteria}" 
+        msg += " after #{condition.offset}" unless condition.offset == ''  
+      end
+      msg
+    end
+    column "Prize" do |condition|
+      condition.prize.description
+    end
+    column :created_at
+    actions
+  end
 
+  # TODO: Hide offset when the condition type is a list
   form do |f|
     inputs 'Conditions' do
       input :prize_id, as: :select, collection: Prize.all.map { |p| [p.description, p.id] }
